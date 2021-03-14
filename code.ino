9489a5fe8ab5ccb0ba80f4f72dcb930b34c1a0d2
Alberu <treblabr@gmail.com>
2021-03-14 11:18:44 +0000

    Add files via upload

--- code.ino
+++ code.ino
@@ -6,7 +6,7 @@ const int buzzerPin = 8; //needs to be changed to the correct value
 // other variables:
 int ledState = LOW; // set the ledState
 int voltage; // just to save the value detected
-int lowerThreshold = 3.07;
+int lowerThreshold = 3.07; // these can be changed to 2.56 and 2.76 for the beding
 int upperThreshold = 3.31;
 
 
@@ -20,7 +20,7 @@ void setup() {
 
 void loop() {
   voltage = digitalRead(inputPin); // check the voltage detected
-  if(voltage > lowerThreshold and voltage < upperThreshold) {
+  if(voltage > lowerThreshold && voltage < upperThreshold) {
     digitalWrite(outputPin, HIGH); // change the LED state to HIGH
     for(i=1; i<=10; i=i+1){
       digitalWrite(buzzerPin, HIGH); // turn buzzer on
