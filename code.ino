b92ff6b774ce9d0b21cb06e3118cf0b006d47c0b
Alberu <treblabr@gmail.com>
2021-03-15 11:53:17 +0000

    Add files via upload

--- code.ino
+++ code.ino
@@ -1,31 +1,34 @@
 // constant variables
 const int inputPin = 12; // input pin for the voltage probe
-const int outputPin = 13; // output pin for LED
-const int buzzerPin = 8; //needs to be changed to the correct value
+const int outputPin = 13; // output pin connected to the LED
+const int buzzerPin = 8; // output pin connected to the buzzer
 
 // other variables:
-int ledState = LOW; // set the ledState
-int voltage; // just to save the value detected
-int lowerThreshold = 3.07; // these can be changed to 2.56 and 2.76 for the beding
+int voltage; // generated now to later be used for the voltage deteced
+// set varialbes for the upper and lower voltage threshold which have been calcualted
+int lowerThreshold = 3.07;
 int upperThreshold = 3.31;
+// these can be changed to 2.56 and 2.76 for the beding
 
 
 void setup() {
   // need to be set to use the same speed serial connection in order to get any sort of intelligible data
   Serial.begin(9600);
-  pinMode(inputPin, INPUT); // set arduino pin to input mode
-  pinMode(outputPin, OUTPUT); // set arduino pin to output mode
-  pinMode(buzzerPin, OUTPUT); // set arduino pin to output mode
+  pinMode(inputPin, INPUT); // set the arduino pin for the probe to input mode
+  pinMode(outputPin, OUTPUT); // set the arduino pin for the LED to output mode
+  pinMode(buzzerPin, OUTPUT); // set the arduino pin for the buzzer to output mode
 }
 
 void loop() {
-  voltage = digitalRead(inputPin); // check the voltage detected
+  voltage = digitalRead(inputPin); // save the probe value deteced
+  //check if the voltage value recorded is between the thresholds values created
   if(voltage > lowerThreshold && voltage < upperThreshold) {
     digitalWrite(outputPin, HIGH); // change the LED state to HIGH
+    // alternate the buzzer bettween on and off
     for(i=1; i<=10; i=i+1){
-      digitalWrite(buzzerPin, HIGH); // turn buzzer on
+      digitalWrite(buzzerPin, HIGH);
       delay(1)
-      digitalWrite(buzzerPin, LOW); // turn buzzer off
+      digitalWrite(buzzerPin, LOW);
     }
   else{
     digitalWrite(outputPin, LOW); // change the LED state to LOW
